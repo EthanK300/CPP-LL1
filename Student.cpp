c03db505795a44b9a9b842dd8585ecea24764816
U-BSD\412185 <412185@SHS-94W2NW3.beaverton.k12.or.us>
2023-11-13 12:23:40 -0800

    continue ll1

---
+++ Student.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Student.h"
+#include <cstring>
+
+using namespace std;
+
+Student::Student(){
+  cout << "Input first name: " << endl;
+  cin.clear();
+  cin >> firstName;
+  cout << "Input last name: " << endl;
+  cin.clear();
+  cin >> lastName;
+  cout << "Input student ID: " << endl;
+  cin.clear();
+  cin >> studentID;
+  cou << "Input student's GPA: " << endl;
+  cin.clear();
+  cin >> GPA;
+  cout << "Added student" << endl;
+  cin.clear();
+}
---
+++ Student.h
@@ -0,0 +1,22 @@
+#ifndef STUDENT
+#define STUDENT
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Student{
+public:
+  char* getFirstName();
+  char* getLastName();
+  int* getStudentID();
+  float* getGPA();
+  void printInfo();
+protected:
+  char firstName[80];
+  char lastName[80];
+  int studentID = 0;
+  float GPA = 0;
+};
+
+#endif
