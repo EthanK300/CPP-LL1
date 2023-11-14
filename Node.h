0695d213207981a1d144fb147eb3f8a0cf78d4f4
EthanK300 <99300748+EthanK300@users.noreply.github.com>
2023-11-14 08:42:42 -0800

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,3 +1,18 @@
+#ifndef NODE
+#define NODE
 #include <iostream>
+#include <cstring>
 
 using namespace std;
+
+class Node{
+public:
+  void setHead(Node* node);
+  void setTail(Node* node);
+protected:
+  Student* student;
+  Node* head;
+  Node* tail;
+}
+
+#endif
